40f4384981910af62d9e62b5ebb514764db00dfc
Venkatesh <vbachu@iserviceglobe.com>
2014-02-01 13:33:01 +0530

    register with admin

--- django_google_plus/admin.py
+++ django_google_plus/admin.py
@@ -1,2 +1,7 @@
-from django.conf import settings
 from django.contrib import admin
+from django_google_plus.models import UserGoogleID
+
+class AdminUserGoogleID(admin.ModelAdmin):
+    model = UserGoogleID
+
+admin.site.register(UserGoogleID, AdminUserGoogleID)
